fa1b5871ba62c522de7a0f2d493faa815830843e
abhay <abhayarrawat28@gmail.com>
2025-07-06 14:23:55 +0530

    fixed vercel-issue

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -21,5 +21,5 @@ app.use('/api/v1/expenses', expenses_1.default);
 app.use('/api/v1/incomes', income_1.default);
 app.use('/api/v1/', ai_1.default);
 app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
+    console.log(`Server is running `);
 });
