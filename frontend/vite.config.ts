2ed6695d6e6ed4f8043866cd8485e56c98568786
abhay <abhayarrawat28@gmail.com>
2025-07-26 03:21:57 +0530

    fixed forntend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from "path"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  build: {
+    chunkSizeWarningLimit: 2000, 
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          
+          vendor: [
+            "react",
+            "react-dom",
+            "react-router-dom",
+            "axios",
+            "lucide-react",
+          ],
+        },
+      },
+    },
+  },
+});
